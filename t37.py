3c46980d9d0b9cfc8ebe5cb30c81693a180d1938
Kirilllord0fgalaxy <tundrasteam31@gmail.com>
2024-09-24 22:37:21 +0300

    Add files via upload

---
+++ t37.py
@@ -0,0 +1,5 @@
+def letters(let):
+    if let.lower()=='y':return 'Гласная-согласная'
+    return 'Гласная' if let.lower() in ['a','e','i','o','u'] else 'Согласная'
+
+print(letters(input('Буква латинского алфавита:')))
---
+++ t38.py
@@ -0,0 +1,9 @@
+def figures(lines):
+    if lines<=2 or lines>10:return 'Нет'
+    nm510=['Пяти','Шести','Cеми','Восьми','Девяти','Десяти']
+    if lines<5 and lines>=3:
+        return 'Треугольник' if lines==3 else 'Прямоугольник/квадрат'
+    else:
+        return nm510[lines-5]+'гранник'
+    
+print(figures(int(input('Кол-во сторон'))))
