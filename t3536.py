c0ba531783424136e25eb0226649ac1ff8ca7f13
Ilya <pankovskiy.im@gmail.com>
2024-09-28 20:11:16 +0300

    correct 2809

--- t3536.py
+++ t3536.py
@@ -8,15 +8,15 @@ def true(num):
 
 def doglife(age):
     res=0
-    if age>0:
+    if age>0:  # из-за такой структуры код с 12 по 18 строчку сдвигается на 4 пробела влево. Попробуй переделать.
         if age<10.5:return res + age/10.5
-        if age-21<0 and age-21>=10.5:
+        if age-21<0 and age-21>=10.5: # зачем это нужно? Попробуй убрать
             res+=1
             age-=10.5
         elif age-21>=0:
             res+=2
             age-=21
-    elif age<0:return'-'
+    elif age<0:return'-' # '-' нельзя считать сообщением об ошибке
     return res + age/4
 
 #Print 36зд
--- t37.py
+++ t37.py
@@ -3,3 +3,4 @@ def letters(let):
     return 'Гласная' if let.lower() in ['a','e','i','o','u'] else 'Согласная'
 
 print(letters(input('Буква латинского алфавита:')))
+# Красивое!
