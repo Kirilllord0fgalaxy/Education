9e488b5ac4622543f72401bf70efcaf30783d433
Kirilllord0fgalaxy <tundrasteam31@gmail.com>
2024-10-14 12:11:34 +0300

    Update t3536.py
    Ну, я вроде исправил, и оно вроде работает

--- t3536.py
+++ t3536.py
@@ -7,16 +7,12 @@ def true(num):
 #36 задача
 
 def doglife(age):
+    if age<0:return 'Ошибка, число отрицательное'
     res=0
-    if age>0:  # из-за такой структуры код с 12 по 18 строчку сдвигается на 4 пробела влево. Попробуй переделать.
-        if age<10.5:return res + age/10.5
-        if age-21<0 and age-21>=10.5: # зачем это нужно? Попробуй убрать
-            res+=1
-            age-=10.5
-        elif age-21>=0:
-            res+=2
-            age-=21
-    elif age<0:return'-' # '-' нельзя считать сообщением об ошибке
+    if age<21:return res + age/10.5
+    elif age-21>=0:
+        res+=2
+        age-=21
     return res + age/4
 
 #Print 36зд
