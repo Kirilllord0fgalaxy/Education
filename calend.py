6e8f18ff445d239f83d79c1938a5653b3efb4a3e
Kirilllord0fgalaxy <tundrasteam31@gmail.com>
2024-11-09 19:16:50 +0300

    End calendar

--- calend.py
+++ calend.py
@@ -6,7 +6,7 @@ def work(stvac,endvac,holidays):
     td = stvac - datetime.timedelta(days=1)
     for _ in range(stvac.day,endvac.day+1):
         td += datetime.timedelta(days=1)
-        if td not in holidays:
+        if td not in holidays and td.weekday() < 5:
             workdays+=1
         print(td)
     return workdays
@@ -15,5 +15,5 @@ def work(stvac,endvac,holidays):
 
 stvac = datetime.datetime.strptime(input('дд.мм.гггг'), '%d.%m.%Y').date()
 endvac = datetime.datetime.strptime(input('дд.мм.гггг'), '%d.%m.%Y').date()
-holidays = [datetime.date(2024, 11, 2),datetime.date(2024, 12, 31)]
+holidays = [datetime.date(2024, 11, 4),datetime.date(2024, 12, 31),datetime.date(2024, 5, 9),datetime.date(2024, 3, 8)]
 print(work(stvac,endvac,holidays))
